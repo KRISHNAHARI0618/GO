fdd749bd52e5d82544cd4c550a5f610fba807f19
KRISHNAHARI0618 <harivardhan9397921834@gmail.com>
2025-04-26 20:24:03 +0530

    26-04-2025

---
+++ Concepts/GO1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world...")
+	a := 42
+	fmt.Println("This is a value :", a)
+	a, b, c, d := 1, "peddireddy", true, 34.67
+	fmt.Println(a, b, c, d)
+}
