b206e3e2d3947111990da737509e16eb0369ca53
KRISHNAHARI0618 <harivardhan9397921834@gmail.com>
2025-04-28 17:11:02 +0000

    28-04-2025 17:10:49

--- Concepts/modules-section/main.go
+++ Concepts/modules-section/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/KRISHNAHARI0618/gomodules"
 )
-func main(){
+
+func main() {
 	fmt.Println("Important Funcion")
 	gomodules.Fascinating()
-	s1 := gomodules.Barks()
-	s2 := gomodules.Bark()
+	s1 := gomodules.Animal()
+
 	fmt.Println(s1)
+
+	s2 := gomodules.Barks()
 	fmt.Println(s2)
 }
